1d0a82231669c8e2a1082e73b5d66efb94ed0264
Oleg Doronin <dorooleg@yandex.ru>
2017-03-27 15:42:52 +0300

    Refactoring dictionary

--- dictionary/src/main/java/DictionaryImpl.java
+++ dictionary/src/main/java/DictionaryImpl.java
@@ -2,16 +2,23 @@
  * Created by user on 03.03.2017.
  */
 public class DictionaryImpl implements Dictionary {
-    
+
+    private static final int INITIAL_HASH_SIZE = 100;
     private int size;
     private List hashTable[];
 
     public DictionaryImpl() {
-        hashTable = new List[100];
-        for (int i = 0; i < hashTable.length; i++) {
-            hashTable[i] = new List();
+        clear();
+    }
+
+    private List getList(String key)
+    {
+        if (key == null) {
+            throw new IllegalArgumentException("Key is invalid argument");
         }
-        size = 0;
+
+        final int hashKey = Math.abs(key.hashCode()) % hashTable.length;
+        return hashTable[hashKey];
     }
 
     @Override
@@ -21,31 +28,21 @@ public class DictionaryImpl implements Dictionary {
 
     @Override
     public boolean contains(String key) {
-        if (key == null) {
-            return false;
-        }
-        final int hashKey = Math.abs(key.hashCode()) % hashTable.length;
-        return hashTable[hashKey].contains(key);
+        return getList(key).contains(key);
     }
 
     @Override
     public String get(String key) {
-        if (key == null) {
-            return null;
-        }
-
-        final int hashKey = Math.abs(key.hashCode()) % hashTable.length;
-        return hashTable[hashKey].get(key);
+        return getList(key).get(key);
     }
 
     @Override
     public String put(String key, String value) {
-        if (key == null || value == null) {
-            return null;
+        if (value == null) {
+            throw new IllegalArgumentException("Value is invalid argument");
         }
 
-        final int hashKey = Math.abs(key.hashCode()) % hashTable.length;
-        String previousValue = hashTable[hashKey].insert(key, value);
+        final String previousValue = getList(key).insert(key, value);
 
         if (previousValue == null) {
             size++;
@@ -60,24 +57,20 @@ public class DictionaryImpl implements Dictionary {
 
     @Override
     public String remove(String key) {
-        if (key == null) {
-            return null;
-        }
-
-        final int hashKey = Math.abs(key.hashCode()) % hashTable.length;
-        String previousValue = hashTable[hashKey].get(key);
-        hashTable[hashKey].removeElement(key);
+        final String previousValue = getList(key).removeElement(key);
 
          if (previousValue != null) {
             size--;
         }
+
         return previousValue;
     }
 
     @Override
     public void clear() {
-        for (List l : hashTable) {
-            l.clear();
+        hashTable = new List[INITIAL_HASH_SIZE];
+        for (int i = 0; i < hashTable.length; i++) {
+            hashTable[i] = new List();
         }
         size = 0;
     }
@@ -104,47 +97,10 @@ public class DictionaryImpl implements Dictionary {
         }
     }
 
-    private class List {
+    private static class List {
 
         private Node root;
         private int size;
-
-        class Pair {
-
-            public String first;
-            public String second;
-
-            Pair(String first, String second) {
-                this.first = first;
-                this.second = second;
-            }
-        }
-
-        class Node {
-            public Node(String key, String value) {
-                this.key = key;
-                this.value = value;
-            }
-            public String key;
-            public String value;
-            public Node next;
-        }
-
-        public void pushBack(String key, String value) {
-            if (root == null) {
-                size++;
-                root = new Node(key, value);
-                return;
-            }
-
-            Node node = root;
-            while (node.next != null) {
-                node = node.next;
-            }
-
-            node.next = new Node(key, value);
-            size++;
-        }
 
         public String insert(String key, String value) {
             Node node = root;
@@ -188,9 +144,9 @@ public class DictionaryImpl implements Dictionary {
             return node == null ? null : node.value;
         }
 
-        public void removeElement(String key) {
+        public String removeElement(String key) {
             if (root == null) {
-                return;
+                return null;
             }
 
             Node previousNode = root;
@@ -206,6 +162,8 @@ public class DictionaryImpl implements Dictionary {
                 if (previousNode == currentNode) root = currentNode.next;
                 else previousNode.next = currentNode.next;
             }
+
+            return currentNode.value;
         }
 
         public Pair[] convertToArray() {
@@ -222,6 +180,29 @@ public class DictionaryImpl implements Dictionary {
 
         public void clear() {
             root = null;
+        }
+
+        static class Node {
+
+            public final String key;
+            public String value;
+            public Node next;
+
+            public Node(String key, String value) {
+                this.key = key;
+                this.value = value;
+            }
+        }
+
+        static class Pair {
+
+            public final String first;
+            public final String second;
+
+            Pair(String first, String second) {
+                this.first = first;
+                this.second = second;
+            }
         }
     }
 }
